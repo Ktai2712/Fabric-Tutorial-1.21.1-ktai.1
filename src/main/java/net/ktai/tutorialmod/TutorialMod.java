846b7e8a0324c4c66ac83734f5a049b97022f282
ktai <tristanedzooi@gmail.com>
2025-08-10 16:16:40 +0200

    Added custom Item groups

--- src/main/java/net/ktai/tutorialmod/TutorialMod.java
+++ src/main/java/net/ktai/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.ktai.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.ktai.tutorialmod.block.ModBlocks;
+import net.ktai.tutorialmod.item.ModItemGroups;
 import net.ktai.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +17,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        ModItemGroups.registerItemGroups();
+
         ModItems.registerModItems();
         ModBlocks.registerModBlocks();
 	}
