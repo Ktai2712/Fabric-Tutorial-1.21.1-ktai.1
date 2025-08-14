1db5bb6355f31da3b641c0c37ec77756258deff4
ktai <tristanedzooi@gmail.com>
2025-08-14 15:21:30 +0200

    added tags to easily add more items to the magicblock ability

--- src/main/java/net/ktai/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/net/ktai/tutorialmod/block/custom/MagicBlock.java
@@ -1,6 +1,7 @@
 package net.ktai.tutorialmod.block.custom;
 
 import net.ktai.tutorialmod.item.ModItems;
+import net.ktai.tutorialmod.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
@@ -34,11 +35,15 @@ public class MagicBlock extends Block {
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if(entity instanceof ItemEntity itemEntity){
-            if(itemEntity.getStack().getItem() == ModItems.RAW_PINK_GARNET ){
+            if(isValidItem(itemEntity.getStack())){
                 itemEntity.setStack(new ItemStack(Items.DIAMOND, itemEntity.getStack().getCount()));
             }
         }
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack stack) {
+        return stack.isIn(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
